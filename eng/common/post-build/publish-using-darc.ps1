b96a30196e8c86d851b7bc8d3fecbacb45a99709
Epsitha Ananth <epananth@microsoft.com>
2020-08-07 17:23:20 -0700

    test publish

--- eng/common/post-build/publish-using-darc.ps1
+++ eng/common/post-build/publish-using-darc.ps1
@@ -1,6 +1,5 @@
 param(
   [Parameter(Mandatory=$true)][int] $BuildId,
-  [Parameter(Mandatory=$true)][int] $PublishingInfraVersion,
   [Parameter(Mandatory=$true)][string] $AzdoToken,
   [Parameter(Mandatory=$true)][string] $MaestroToken,
   [Parameter(Mandatory=$false)][string] $MaestroApiEndPoint = 'https://maestro-prod.westus2.cloudapp.azure.com',
@@ -50,7 +49,7 @@ try {
   }
 
   & darc add-build-to-channel `
-  --id $buildId `
+	--id $buildId `
   --publishing-infra-version $PublishingInfraVersion `
 	--default-channels `
 	--source-branch master `
